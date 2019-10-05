06e7d05d7bd4631779e9c3306d8a7a559b2a5cdc
Neil Nie <yongyang.nie@gmail.com>
2019-10-05 00:02:16 -0400

    initial commit

---
+++ build/simple_number_pub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/neil/Desktop/ROS-tutorial/src/simple_number_pub
+# Build directory: /home/neil/Desktop/ROS-tutorial/build/simple_number_pub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
