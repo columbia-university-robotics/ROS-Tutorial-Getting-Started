2f05203a5b4d0d09cff6cfbd1b8f9144b046d4a9
Neil Nie <yongyang.nie@gmail.com>
2019-10-09 17:56:10 -0400

    build and devel dirs removed

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neil/Desktop/ROS-tutorial/src
-# Build directory: /home/neil/Desktop/ROS-tutorial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("vision")
-subdirs("simple_number_pub")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/neil/Desktop/ROS-tutorial/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/neil/Workspace/self-driving-golf-cart/ros/devel;/opt/ros/melodic".split(';') if "/home/neil/Workspace/self-driving-golf-cart/ros/devel;/opt/ros/melodic" != "" else []
